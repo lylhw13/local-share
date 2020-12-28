19390e03e6adb134c2b1d04dae9593fd78bb9925
lylhw13 <lylhw13@gmail.com>
2020-12-28 20:36:17 +0800

    change directory

--- message_window/colors.js
+++
@@ -1,6 +0,0 @@
-const COLORS = ["red", "pink", "purple", "deep-purple", "indigo", "blue", "light-blue", "cyan", "teal", "green", "light-green", "lime", "yellow", "amber",
-                "orange", "deep-orange", "brown"]
-
-export function pickcolor() {
-    return COLORS[Math.floor(Math.random() * COLORS.length)];
-}
